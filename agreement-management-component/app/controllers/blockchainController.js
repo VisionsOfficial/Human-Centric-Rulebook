12603ddce9e63c3834d48964eb0b98a451d0b3bd
Felix-Visions <felix@visionspol.eu>
2021-09-02 20:23:14 +0200

    feat: Add dataset request endpoints and logic

--- agreement-management-component/app/controllers/blockchainController.js
+++ agreement-management-component/app/controllers/blockchainController.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 
 const auth = require("../config/token");
+const DataSharingContractModel = require("../models/DataSharingContract.model");
 
 // const ENDPOINT = "https://fdodmap3zh.execute-api.eu-central-1.amazonaws.com/test";
 const ENDPOINT = "https://y5u9ap15bi.execute-api.us-east-1.amazonaws.com/test";
@@ -31,8 +32,18 @@ exports.uploadClientSignature = async (req, res, next) => {
 	const hash = req.body.hash;
 	// [Required] a string representing a signature of the signed message
 	const signature = req.body.signature;
+	// Ethereum wallet address of the signing party
+	const address = req.body.address;
 
-	const token = auth.generateBlockchainToken(contractId, true);
+	const token = auth.generateBlockchainToken(address, contractId, true);
+
+	const contract = await DataSharingContractModel.findById(contractId);
+
+	if (!contract) {
+		return res
+			.status(404)
+			.json({ error: "not-found-error", message: "Contract ID not found" });
+	}
 
 	axios({
 		method: "POST",
@@ -46,9 +57,24 @@ exports.uploadClientSignature = async (req, res, next) => {
 			Authorization: token,
 		},
 	})
-		.then((data) => {
-			console.log(data.data);
-			return res.status(data.status).json(data.data);
+		.then(async (response) => {
+			console.log(response.data);
+
+			// Update DSA with user signature
+			contract.dataUserSignature = {
+				signed: true,
+				timestamp: Date.now(),
+			};
+
+			await contract.save();
+
+			console.log(contract);
+
+			return res.status(200).json({
+				blokchainResponse: response.data,
+				message: "Data user signature successfully saved.",
+				contract: contract,
+			});
 		})
 		.catch((err) => {
 			console.log(err.response.data.errors);
@@ -65,8 +91,18 @@ exports.uploadProviderSignature = async (req, res, next) => {
 	const hash = req.body.hash;
 	// [Required] a string representing a signature of the signed message
 	const signature = req.body.signature;
+	// Ethereum wallet address of the signing party
+	const address = req.body.address;
 
-	const token = auth.generateBlockchainToken(contractId, false);
+	const contract = await DataSharingContractModel.findById(contractId);
+
+	if (!contract) {
+		return res
+			.status(404)
+			.json({ error: "not-found-error", message: "Contract ID not found" });
+	}
+
+	const token = auth.generateBlockchainToken(address, contractId, false);
 
 	axios({
 		method: "POST",
@@ -80,11 +116,52 @@ exports.uploadProviderSignature = async (req, res, next) => {
 			Authorization: token,
 		},
 	})
-		.then((data) => {
-			console.log(data.data);
-			return res.status(data.status).json(data.data);
+		.then(async (response) => {
+			console.log(response.data);
+
+			// Update DSA with provider signature
+			contract.dataProviderSignature = {
+				signed: true,
+				timestamp: Date.now(),
+			};
+
+			await contract.save();
+
+			console.log(contract);
+
+			return res.status(200).json({
+				blokchainResponse: response.data,
+				message: "Data provider signature successfully saved.",
+				contract: contract,
+			});
 		})
 		.catch((err) => {
 			return res.json(err);
 		});
+};
+
+exports.verify = async (req, res, next) => {
+	try {
+		const contractId = req.body.contractId;
+
+		// const token = auth.generateBlockchainToken(address, contractId, false);
+
+		axios({
+			method: "GET",
+			url: ENDPOINT + "/contracts/" + contractId + "/verify",
+			headers: {
+				"Content-Type": "application/json; charset=utf-8",
+			},
+		})
+			.then((response) => {
+				console.log(response.data.data);
+				return res.status(response.status).json(response.data);
+			})
+			.catch((err) => {
+				return res.json(err);
+			});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ error: "internal-server-error" });
+	}
 };
--- agreement-management-component/app/routes/blockchain.js
+++ agreement-management-component/app/routes/blockchain.js
@@ -7,5 +7,6 @@ const ctrl = require("../controllers/blockchainController");
 router.get("/contracts", ctrl.getContracts);
 router.post("/contracts/:contractId/sign/client", ctrl.uploadClientSignature);
 router.post("/contracts/:contractId/sign/provider", ctrl.uploadProviderSignature);
+router.post("/verify", ctrl.verify);
 
 module.exports = router;
